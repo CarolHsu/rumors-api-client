2219ce783e75a97b6c53443f2ca49158395bb0e4
Tomo Hung <tomohung@gmail.com>
2018-12-28 08:43:27 +0800

    use Object.const_get instead of eval

--- lib/rumors/api/client/base.rb
+++ lib/rumors/api/client/base.rb
@@ -85,7 +85,7 @@ module Rumors
 
         def build_body(util, argument)
           current_util_class = "rumors/api/client/utils/#{util}".classify
-          current_util = eval(current_util_class).new(argument)
+          current_util = Object.const_get(current_util_class).new(argument)
           {
             query: current_util.purify_gql_query,
             variables: current_util.variables
